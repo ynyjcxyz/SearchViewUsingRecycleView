9356ae6b8c0eb7f1f2e33b49f1266f48ef47f01e
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-06 12:41:04 -0700

    Break the unit test by changing the MiniEntity model.

--- app/src/main/java/com/example/android/searchlist/MiniEntity.java
+++ app/src/main/java/com/example/android/searchlist/MiniEntity.java
@@ -3,19 +3,21 @@ package com.example.android.searchlist;
 import com.google.auto.value.AutoValue;
 import com.google.gson.annotations.SerializedName;
 import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
+import java.util.Collections;
+import java.util.List;
 
 @GenerateTypeAdapter
 @AutoValue
 public abstract class MiniEntity {
 
   @SerializedName("id")
-  abstract Integer id();
+  abstract List<String> id();
 
   @SerializedName("name")
   abstract String name();
 
   public static MiniEntity create(Integer id, String name) {
-    return new AutoValue_MiniEntity(id, name);
+    return new AutoValue_MiniEntity(Collections.emptyList(), name);
   }
 
 }
