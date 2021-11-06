5afef1d21998ff873be10513219c698e9670688f
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-06 12:27:00 -0700

    Add txt read example and json example

--- app/src/test/java/com/example/android/searchlist/FileReaderTest.java
+++ app/src/test/java/com/example/android/searchlist/FileReaderTest.java
@@ -2,12 +2,30 @@ package com.example.android.searchlist;
 
 import com.google.common.truth.Truth;
 import org.junit.Test;
+import util.TestUtil;
 
 
 public class FileReaderTest {
 
-  @Test
-  public void testReadFile() {
+  private static final String JSON_EXAMPLE = "{\n"
+      + "  \"id\": 1,\n"
+      + "  \"name\": \"Tony\"\n"
+      + "}";
 
-   }
+  @Test
+  public void testReadJsonFile() {
+
+    String jsonContent = TestUtil.content("mini.json", this);
+    System.out.println(jsonContent);
+    Truth.assertThat(jsonContent).isEqualTo(JSON_EXAMPLE);
+  }
+
+
+  @Test
+  public void testReadTxtFile() {
+
+    String txtContent = TestUtil.content("1.txt", this);
+    Truth.assertThat(txtContent).isEqualTo("123");
+
+  }
 }
