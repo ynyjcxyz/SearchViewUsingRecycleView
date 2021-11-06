e2fdf90895acdb51ba251fb0aa40c6ffe4ce7d08
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-06 13:29:23 -0700

    Add log MovieApplication

--- app/src/main/java/com/example/android/searchlist/MovieApplication.java
+++ app/src/main/java/com/example/android/searchlist/MovieApplication.java
@@ -1,12 +1,18 @@
 package com.example.android.searchlist;
 
 import android.app.Application;
+import android.util.Log;
 
 public class MovieApplication extends Application {
+
+  private static final String LOG_TAG = "AppStart";
 
   @Override
   public void onCreate() {
     super.onCreate();
+    Log.i(LOG_TAG,"This is first statement on app executing");
+    //it is equals public static final void main(String[] args);
     AppInstanceUtil.setInstance(this);
   }
-}
+
+ }
